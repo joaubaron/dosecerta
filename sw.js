d14380089cdcc8e799720948fcdb47804c7cca2e
joaubaron <joaubaron@users.noreply.github.com>
2026-04-24 13:56:46 +0000

    deploy: 1a5ce3aa79627d622c0aef1554248cc67c6d7a4d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '24.04.2026-1050';
+const CACHE_VERSION = '24.04.2026-1056';
 const CACHE_NAME = `medlembrar-${CACHE_VERSION}`;
 const ASSETS = ['./index.html', './manifest.json'];
 
